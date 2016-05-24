60db665485b114609086ac4048da19d42f37cdc1
Jenny <bebetterjenny@gmail.com>
2016-05-23 23:05:11 -0400

    Add files via upload

---
+++ Jo_insert.vb
@@ -0,0 +1,64 @@
+Sub Jo_insert_air()
+'
+' Jo_insert1 Macro
+'
+
+'
+    ActiveCell.Rows("1:1").EntireRow.Select
+    Selection.Insert Shift:=xlDown, CopyOrigin:=xlFormatFromLeftOrAbove
+    ActiveCell.FormulaR1C1 = "=R[1]C[3] & CHAR(10) & R[1]C[2]"
+    ActiveCell.Offset(0, 0).Range("A1").Select
+    With Selection.Interior
+        .Pattern = xlSolid
+        .PatternColorIndex = xlAutomatic
+        .ThemeColor = xlThemeColorAccent5
+        .TintAndShade = 0
+        .PatternTintAndShade = 0
+    End With
+    ActiveCell.Offset(0, 0).Range("A1").Select
+    Selection.Copy
+    Selection.PasteSpecial Paste:=xlPasteValues, Operation:=xlNone, SkipBlanks _
+        :=False, Transpose:=False
+    Application.CutCopyMode = False
+    ActiveCell.Offset(0, 1).Range("A1").Select
+    With Selection.Interior
+        .Pattern = xlNone
+        .TintAndShade = 0
+        .PatternTintAndShade = 0
+    End With
+    ActiveCell.Offset(2, -1).Range("A1").Select
+    
+
+End Sub
+Sub Jo_insert_sea()
+'
+' Jo_insert1 Macro
+'
+
+'
+    ActiveCell.Rows("1:1").EntireRow.Select
+    Selection.Insert Shift:=xlDown, CopyOrigin:=xlFormatFromLeftOrAbove
+    ActiveCell.FormulaR1C1 = "=R[1]C[3] & CHAR(10) & R[1]C[2]"
+    ActiveCell.Offset(0, 0).Range("A1").Select
+    With Selection.Interior
+        .Pattern = xlSolid
+        .PatternColorIndex = xlAutomatic
+        .ThemeColor = xlThemeColorAccent6
+        .TintAndShade = 0
+        .PatternTintAndShade = 0
+    End With
+    ActiveCell.Offset(0, 0).Range("A1").Select
+    Selection.Copy
+    Selection.PasteSpecial Paste:=xlPasteValues, Operation:=xlNone, SkipBlanks _
+        :=False, Transpose:=False
+    Application.CutCopyMode = False
+    ActiveCell.Offset(0, 1).Range("A1").Select
+    With Selection.Interior
+        .Pattern = xlNone
+        .TintAndShade = 0
+        .PatternTintAndShade = 0
+    End With
+    ActiveCell.Offset(2, -1).Range("A1").Select
+    
+
+End Sub
