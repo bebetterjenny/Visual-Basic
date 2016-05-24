c5aa058306258570d6a353635839e9dc50275354
Jenny <bebetterjenny@gmail.com>
2016-05-23 23:06:35 -0400

    Update commonUse.vb

--- commonUse.vb
+++ commonUse.vb
@@ -52,6 +52,21 @@ Loop
 
 
 
+Sub insertCopy()
+
+ActiveCell.Rows("1:1").EntireRow.Select
+Selection.Insert Shift:=xlDown, CopyOrigin:=xlFormatFromLeftOrAbove
+ActiveCell.FormulaR1C1 = "=R[1]C[0]"
+ActiveCell.Offset(0, 0).Range("A1").Select
+Selection.Copy
+Selection.PasteSpecial Paste:=xlPasteValues, Operation:=xlNone, SkipBlanks _
+    :=False, Transpose:=False
+Application.CutCopyMode = False
+
+End Sub
+
+
+
 
 End Sub
 
